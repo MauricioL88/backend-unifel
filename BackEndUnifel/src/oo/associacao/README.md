6210faeee0311df93f430ff7f489de7502e8fcf6
MauricioL88 <lisboa.sse@gmail.com>
2024-11-16 21:30:13 -0300

    Adicionando a classe Cliente do exemple de associação

--- BackEndUnifel/src/oo/associacao/README.md
+++ BackEndUnifel/src/oo/associacao/README.md
@@ -1,18 +1,35 @@
-# Estudo de Associações na Orientação à Objetos
+# Estudo de Relacionamentos entre as classes na Orientação à Objetos
 
-## Simples
-* Quando duas ou mais classes se relacionam mas não há dependencia entre elas para
-existirem;
+## Associação Simples
+* Quando duas classes se relacionam (se utilizam, uma dentro da outra ou como parâmetro)
+mas não há dependencia entre elas para existirem;
+* No diagrama de classe essa relação é representada por uma linha simples;
 
 ex:
 <br>
-* As classes Cliente e Conta do pacote "simples" deste estudo;
-* No diagrama de classe essa relação é representada por uma linha simples:
 
 ```mermaid
 
 classDiagram
-Cliente --> Conta
+class Conta {
+    +static num : int
+    -conta : Integer
+    -agencia : String
+    -cliente : Cliente
+    -saldo : Double
+}
+
+class Cliente {
+    +static num : int
+    -id : Integer
+    -nome : String
+    -cpf : String
+    -anoNascimento : Integer
+    -email : String
+}
+
+
+Cliente -- Conta
 
 ```
 
--- BackEndUnifel/src/oo/associacao/simples/Cliente.java
+++ BackEndUnifel/src/oo/associacao/simples/Cliente.java
@@ -1,5 +1,67 @@
 package oo.associacao.simples;
 
 public class Cliente {
+    public static int num = 1;
     
+    private Integer id;
+    private String nome;
+    private String cpf;
+    private Integer anoNascimento;
+    private String email;
+    
+    public Cliente(String nome, String cpf, Integer anoNascimento, String email) {
+        this.id = Cliente.num++;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.anoNascimento = anoNascimento;
+        this.email = email;
+    }
+
+    public static int getNum() {
+        return num;
+    }
+
+    public static void setNum(int num) {
+        Cliente.num = num;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Integer getAnoNascimento() {
+        return anoNascimento;
+    }
+
+    public void setAnoNascimento(Integer anoNascimento) {
+        this.anoNascimento = anoNascimento;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
